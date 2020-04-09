bc92f91f162cd9088ab33d59d586e17ad4d6aec0
Fede <federico.andres.perez@fing.edu.uy>
2020-04-09 20:02:30 -0300

    Mostrando el resumen

--- src/components/Resumen.js
+++ src/components/Resumen.js
@@ -1,4 +1,14 @@
-import React, {Fragment} from 'react';
+import React from 'react';
+import styled from '@emotion/styled';
+import {primerMayuscula} from '../helper'
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00838F;
+    color: #FFF;
+    margin-top: 1rem;
+`;
 
 const Resumen = ({datos}) => {
     
@@ -10,14 +20,14 @@ const Resumen = ({datos}) => {
     }
 
     return ( 
-        <Fragment>
-        <h2>Resumen de cotización</h2>
-        <ul>
-            <li>Marca: </li>
-            <li>Plan: </li>
-            <li>Año del Auto: </li>
-        </ul>
-        </Fragment>
+        <ContenedorResumen>
+            <h2>Resumen de cotización</h2>
+            <ul>
+                <li>Marca: {primerMayuscula(marca)}</li>
+                <li>Plan: {primerMayuscula(plan)}</li>
+                <li>Año del Auto: {year}</li>
+            </ul>
+        </ContenedorResumen>
      );
 }
  
